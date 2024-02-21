62184ff03ef4f383a29fd0f13026ad0ddc63b294
PrasadKale18 <prasadkale932@gmail.com>
2024-02-21 12:08:23 +0530

    first commit

---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+function LoginSignup() {
+    const [data,setData]=useState("Login")
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{data}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {data==="Login"?<div></div>:<div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name"/>
+        </div>}
+        
+      </div>
+      <div className="inputs">
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email Id"/>
+        </div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password"/>
+        </div>
+      </div>
+      {data==="Sign Up"?<div></div>:<div className="forgot-password">Forget Password? <span>Click Here!</span></div>}
+
+      <div className="submit-container">
+        <div className={data==="Login"?"submit gray":"submit"} onClick={()=>{setData("Sign Up")}}>Sign Up</div>
+        <div className={data==="Sign Up"?"submit gray":"submit"} onClick={()=>{setData("Login")}}>Login</div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSignup;
